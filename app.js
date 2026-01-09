2c3002a1783810e146c8fbd5c839a1e66c8de93d
oumaima <oumaima.elhabib@gmail.com>
2026-01-09 14:30:31 +0100

    fix error app

--- app.js
+++ app.js
@@ -397,8 +397,6 @@ module.exports = app;
 if (require.main === module) {
     const port = process.env.PORT || 5000;
     app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
+        console.log(`Server running locally on port ${port}`);
     });
 }
-
-});
